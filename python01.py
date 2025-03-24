bd86f05b6c1db5fe1330598d0681209c56be93c3
ShahJahanBro <shahjahan12098@gmail.com>
2025-03-24 23:58:53 +0500

    A simple python file

---
+++ python01.py
@@ -0,0 +1,118 @@
+# import sys
+# print(sys.version)
+
+# print("Hello World")
+# print("Shah Jahan", 21)
+# print("Abbas " + "Yasin")
+# print("Ahad","Baqar")
+# # for adding comments in Python Programming 
+# print(1,2,3, sep=" ~ ")
+# print("Shah Nawaz", "Shah Jahan" , "Abbas" , sep=" ~ ")
+# print("Shah Nawaz", "Shah Jahan" , "Abbas" ,  end=" ~\n")
+# print("Learning Pyton is Fun...",)
+
+
+# This is a comment: More readable, code explain
+
+"""
+This is also a commnet
+but more than one line comment
+"""
+
+# Python has no command for declaring a variable.
+
+name = "Shah jahan"
+# print(name)
+
+# print("Type of name : ", type(name))
+
+# # Type can be changed
+# name = 15
+# print(name)
+# print("Type of name : ", type(name))
+
+# age = int(22)
+# name = str("shahjahan")
+# num = float(3)
+
+# print(age, name, num)
+# this is called casting
+
+# NOTE: Variables names are case sensitive
+
+# x = "Python"
+# y = "is"
+# z = " awesome"
+# print(x, y, z)
+# print(x + y + z) 
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+
+# def myfunc():
+#   global y
+#   y = "fantastic"
+
+# myfunc()
+
+# print("Python is " + y)
+
+
+
+# z = "awesome"
+
+# def myfunc():
+#   global z
+#   z = "fantastic"
+
+# myfunc()
+
+# print("Python is " + z)
+
+# # we can also change the scope of a variable as in the above examples
+
+
+# num = 5.0
+
+# print(num)
+
+# print(complex(num))
+
+# Note: You cannot convert complex numbers into another number type.
+
+name = "9"
+print(name)
+print(int(name))
+
+
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+# Python does not have a 'character' data type, a single character is simply a string with a length of 1.
+
+name = "Shah jahan"
+# print(len(name)) # spaces are also counted either on the left or right of the string
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+
+print(name[1:2])
+print(name[-3:-1])
+
+# Python supports negative indices where -1 represents the last element, -2 represents the second last element, and so on.
+
+print(name.replace(" ", "-"))
