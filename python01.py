646912a4eefb79023482f31539f37cefb11f00aa
ShahJahanBro <shahjahan12098@gmail.com>
2025-03-25 01:32:53 +0500

    Added a new python file to experiment pull more

--- python01.py
+++ python01.py
@@ -24,101 +24,101 @@ but more than one line comment
 name = "Shah jahan"
 # print(name)
 
-# print("Type of name : ", type(name))
+# # print("Type of name : ", type(name))
 
-# # Type can be changed
-# name = 15
+# # # Type can be changed
+# # name = 15
+# # print(name)
+# # print("Type of name : ", type(name))
+
+# # age = int(22)
+# # name = str("shahjahan")
+# # num = float(3)
+
+# # print(age, name, num)
+# # this is called casting
+
+# # NOTE: Variables names are case sensitive
+
+# # x = "Python"
+# # y = "is"
+# # z = " awesome"
+# # print(x, y, z)
+# # print(x + y + z) 
+
+# # x = "awesome"
+
+# # def myfunc():
+# #   x = "fantastic"
+# #   print("Python is " + x)
+
+# # myfunc()
+
+# # print("Python is " + x)
+
+
+# # def myfunc():
+# #   global y
+# #   y = "fantastic"
+
+# # myfunc()
+
+# # print("Python is " + y)
+
+
+
+# # z = "awesome"
+
+# # def myfunc():
+# #   global z
+# #   z = "fantastic"
+
+# # myfunc()
+
+# # print("Python is " + z)
+
+# # # we can also change the scope of a variable as in the above examples
+
+
+# # num = 5.0
+
+# # print(num)
+
+# # print(complex(num))
+
+# # Note: You cannot convert complex numbers into another number type.
+
+# name = "9"
 # print(name)
-# print("Type of name : ", type(name))
-
-# age = int(22)
-# name = str("shahjahan")
-# num = float(3)
-
-# print(age, name, num)
-# this is called casting
-
-# NOTE: Variables names are case sensitive
-
-# x = "Python"
-# y = "is"
-# z = " awesome"
-# print(x, y, z)
-# print(x + y + z) 
-
-# x = "awesome"
-
-# def myfunc():
-#   x = "fantastic"
-#   print("Python is " + x)
-
-# myfunc()
-
-# print("Python is " + x)
-
-
-# def myfunc():
-#   global y
-#   y = "fantastic"
-
-# myfunc()
-
-# print("Python is " + y)
+# print(int(name))
 
 
 
-# z = "awesome"
-
-# def myfunc():
-#   global z
-#   z = "fantastic"
-
-# myfunc()
-
-# print("Python is " + z)
-
-# # we can also change the scope of a variable as in the above examples
+# a = '''Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.'''
+# print(a)
 
 
-# num = 5.0
+# # Python does not have a 'character' data type, a single character is simply a string with a length of 1.
 
-# print(num)
-
-# print(complex(num))
-
-# Note: You cannot convert complex numbers into another number type.
-
-name = "9"
-print(name)
-print(int(name))
+# name = "Shah jahan"
+# # print(len(name)) # spaces are also counted either on the left or right of the string
+# # print(name[1])
+# # print(name[2])
+# # print(name[3])
 
 
+# print(name[1:2])
+# print(name[-3:-1])
 
-a = '''Lorem ipsum dolor sit amet,
-consectetur adipiscing elit,
-sed do eiusmod tempor incididunt
-ut labore et dolore magna aliqua.'''
-print(a)
+# # Python supports negative indices where -1 represents the last element, -2 represents the second last element, and so on.
 
+# print(name.replace(" ", "-"))
+# #This is has been pushed to gitHub now...
 
-# Python does not have a 'character' data type, a single character is simply a string with a length of 1.
+# # yeah
+# # This has been pulled again and made some changes to check
 
-name = "Shah jahan"
-# print(len(name)) # spaces are also counted either on the left or right of the string
-# print(name[1])
-# print(name[2])
-# print(name[3])
-
-
-print(name[1:2])
-print(name[-3:-1])
-
-# Python supports negative indices where -1 represents the last element, -2 represents the second last element, and so on.
-
-print(name.replace(" ", "-"))
-#This is has been pushed to gitHub now...
-
-# yeah
-# This has been pulled again and made some changes to check
-
-# go and check the changes
+# # go and check the changes
